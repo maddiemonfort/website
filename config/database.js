ee9d2547e2d909d360fb62abe60010f606a09509
Zavum <dakfunchess@gmail.com>
2016-02-16 11:33:33 -0500

    first commit

---
+++ config/database.js
@@ -0,0 +1,5 @@
+module.exports = {
+
+    'url' : ' mongodb://zavum:koolkittykat@ds011278.mongolab.com:11278/logincredentials' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
